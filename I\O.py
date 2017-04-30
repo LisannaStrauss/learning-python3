109d39e92bf3e78ba5968b923ffd21460107b243
LisannaStrauss <610696333@qq.com>
2017-04-30 22:46:18 +0800

    Update I\O.py

--- I\O.py
+++ I\O.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 
 #input()函数是基础的输入函数，可以（）内写入要求用户输入的提示信息，用户输入的信息可以有一个变量接收，数据类型：字符串
-name = input('Please enter your name:')
+name = input('Please enter your name:')
 #print()函数是基础的输出函数，可以将字符串输出至控制台
 print('Hello',name)
  
